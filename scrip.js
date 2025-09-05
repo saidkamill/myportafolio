b4f08ef89e0b407c177923739f55887e6d2095fb
said <saidckamil@gmail.com>
2025-09-05 19:34:56 +0300

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -6,3 +6,16 @@ document.addEventListener('DOMContentLoaded', function() {
   const el = document.getElementById('year');
   if (el) el.textContent = y;
 });
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("viewAllBtn");
+  const hiddenItems = document.querySelectorAll(".extra-work");
+  let expanded = false;
+
+  btn.addEventListener("click", () => {
+    hiddenItems.forEach(item => {
+      item.classList.toggle("hidden");
+    });
+    expanded = !expanded;
+    btn.textContent = expanded ? "Show Less" : "View All";
+  });
+});
